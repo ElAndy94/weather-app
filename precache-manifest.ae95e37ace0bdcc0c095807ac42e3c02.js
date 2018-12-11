10cc0cce8c26e5012440ecd8a054da0a6e1a8495
Andrew <andrewpeliza@hotmail.com>
2018-12-11 14:16:36 +0000

    Updates

---
+++ precache-manifest.ae95e37ace0bdcc0c095807ac42e3c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4b940ec5c00adf6c021",
+    "url": "/weather-app/static/css/main.f01512e3.chunk.css"
+  },
+  {
+    "revision": "a4b940ec5c00adf6c021",
+    "url": "/weather-app/static/js/main.a4b940ec.chunk.js"
+  },
+  {
+    "revision": "4021b4288df3dbe5e858",
+    "url": "/weather-app/static/js/1.4021b428.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "603eca3af6405fef24e9937a123f3c32",
+    "url": "/weather-app/static/media/bg.603eca3a.jpg"
+  },
+  {
+    "revision": "a8b7616065a9c2042bd5813d15c74e7a",
+    "url": "/weather-app/index.html"
+  }
+];
